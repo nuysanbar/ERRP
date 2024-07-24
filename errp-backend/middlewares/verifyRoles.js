4a74d4d8e3d2807153562d6c286cc3985bcfe578
Nuysanbar <102357504+nuysanbar@users.noreply.github.com>
2024-07-24 09:28:30 +0300

    Cleaned verifyRoles.js

--- errp-backend/middlewares/verifyRoles.js
+++ errp-backend/middlewares/verifyRoles.js
@@ -1,7 +1,6 @@
 const verifyRoles=(...allowedRoles)=>{
     return (req,res,next)=>{
         if(!req?.roles) return res.sendStatus(401);
-        console.log(req.roles);
         const rolesArray=[...allowedRoles];
         console.log(rolesArray);
         const result = req.roles.map(role=>rolesArray.includes(role)).find(val=>val===true);
