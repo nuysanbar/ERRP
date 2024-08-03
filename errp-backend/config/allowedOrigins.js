41356db986ff6a3d4c51cfdf05a510a8302d322f
Nuysanbar <102357504+nuysanbar@users.noreply.github.com>
2024-08-03 21:40:53 +0300

    Removed change allowedOrigins.js

--- errp-backend/config/allowedOrigins.js
+++ errp-backend/config/allowedOrigins.js
@@ -1,8 +1,6 @@
 var allowlist = ['http://www.google.com', 
 'http://localhost:3500/',
 'http://localhost:3000',
-'https://test.yenepay.com/',
-'https://localhost:5000/',
-                 
+'https://test.yenepay.com/'              
 ]
 module.exports=allowlist;
