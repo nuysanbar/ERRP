e92939c14df5587e6a09e3c5763d88be09b18aa7
Nuysanbar <102357504+nuysanbar@users.noreply.github.com>
2024-08-03 21:42:05 +0300

    Unnecessary comment error-page.jsx

--- errp-frontend/src/error-page.jsx
+++ errp-frontend/src/error-page.jsx
@@ -2,9 +2,6 @@ import { useRouteError } from "react-router-dom";
 import Alert from '@mui/material/Alert';
 export default function ErrorPage({children}) {
   const error = useRouteError();
-  console.error(error);
- //output error messages
-
   return (
     <div id="error-page">
       {error.response.data.message && (<div style={{color:"red", textAlign:"center",width:"70%",margin:"10px auto 0"}}>
